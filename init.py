efc2d16e70214722da51c2ee431757b4039f22dc
Jens <jens.potthoff98@yahoo.de>
2024-04-21 15:50:27 +0200

    Added Info togglebutton

--- init.py
+++ init.py
@@ -7,6 +7,7 @@ init = st.session_state
 
 
 def initVars():
+
     init.writeInDocs = st.session_state.get('writeInDocs', False)
     init.download_Model = st.session_state.get('download_Model', False)
 
@@ -46,34 +47,15 @@ def initVars():
 
 
 
-
-
-
-
-
-
-
-
-
-
     init.model_updated = st.session_state.get('model_updated', False)
     init.writeInDocs = st.session_state.get('writeInDocs', False)
     init.rag = st.session_state.get('rag', False)
-    init.template = st.session_state.get('template', "")
 
     init.endInstruction = st.session_state.get('endInstruction', "")
     init.notInfo = st.session_state.get('notInfo', "")
 
     init.usechatMemory = st.session_state.get('usechatMemory', "") 
     
-
-
-
-
-
-
-
-
 
 
     init.llm = st.session_state.get('llm', None)
@@ -83,55 +65,24 @@ def initVars():
     init.prompt = st.session_state.get('prompt', "")
 
 
-
-
-
-
-
-
-
-
-
     init.selectedFile = st.session_state.get('selectedFile', "")
     init.selected_index = st.session_state.get('selected_index', 0)
     init.embeddings = st.session_state.get('embeddings', None)
 
 
-
-
-
-
-    init.system_prompt = st.session_state.get('system_prompt', " ")
-
-
-
-
     init.writeInDocs = st.session_state.get('writeInDocs', False)
-
-
 
     init.historyTemplateUSER = st.session_state.get('historyTemplateUSER', "")
     init.historyTemplateBOT = st.session_state.get('historyTemplateBOT', "")
 
 
-
+    init.system_prompt = st.session_state.get('system_prompt', " ")
     init.systemPrompt = st.session_state.get('systemPrompt', "")
-
     init.imageUpload = st.session_state.get('imageUpload', None)
-
     init.file_path = st.session_state.get('file_path', "")
-
     init.vision = st.session_state.get('vision', False)
-
     init.mmprojFileFound = st.session_state.get('mmprojFileFound', False)
-
     init.chat_handler = st.session_state.get('chat_handler', None)
-
-
-
-
-
-
 
 
 
@@ -139,13 +90,11 @@ def initVars():
 
     # config
         
-    #Sprachmodell-Pfad
     init.repo_id = st.session_state.get('repo_id', 'TheBloke/SauerkrautLM-3B-v1-GGUF')
     init.model_file_name = st.session_state.get('model_file_name', 'sauerkrautlm-3b-v1.Q8_0.gguf')
     init.model_path = st.session_state.get('model_path', None)
 
 
-    #Modellparameter
     init.gpu_layer = st.session_state.get('gpu_layer', 20)
     init.temperature = st.session_state.get('temperature', 0.25)
     init.max_Token = st.session_state.get('max_Token', 512)
@@ -156,13 +105,10 @@ def initVars():
     var = ("[INST]", "### Assistant", "<eoa>", "<|im_end|>")
 
 
-    #Embedding-Parameter
     init.topk = st.session_state.get('topk', 4)
     init.chunk_size = st.session_state.get('chunk_size', 600)
     init.overlap = st.session_state.get('overlap', 50)
     init.Euklidischer_Abstand = st.session_state.get('Euklidischer_Abstand', 3.5)
-
-
 
 
     init.download = st.session_state.get('download', False)
@@ -170,13 +116,10 @@ def initVars():
 
 
     init.RAGTopic = st.session_state.get('RAGTopic', " ")
-
     init.RAGFilePath = st.session_state.get('RAGFilePath', " ")
-
     init.search = st.session_state.get('search', False)
-
     init.memUsageThread = st.session_state.get('memUsageThread', False)
-
-
     init.downloadStart = st.session_state.get('downloadStart', False)
+
+    init.dayDateInfo = st.session_state.get('dayDateInfo', False)
 
